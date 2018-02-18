939ed8c6fb41b5da09325b6e28868b9eb310a3c5
erictenenbaum <erictenenbaum10@gmail.com>
2018-02-18 16:54:51 -0700

    added different path

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/"
+    callbackURL: "/home"
   },
   function(accessToken, refreshToken, profile, done) {
        User.findOrCreate({ googleId: profile.id }, function (err, user) {
